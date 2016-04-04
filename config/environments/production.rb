78e4a0a48cad96167da013717761c5ec0ba110d0
John Ivens <john.wagner.ivens@gmail.com>
2016-04-04 23:22:00 +0000

    Setup email functionality for sign-up

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,6 +9,11 @@ Rails.application.configure do
   # and those relying on copy on write to perform better.
   # Rake tasks automatically ignore this option for performance.
   config.eager_load = true
+  
+  # Setup production mailer so that mail goes to the actual email address for heroku
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { :host => 'jivens-photo-app.herokuapp.com', :protocol => 'https' }
+  
 
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
